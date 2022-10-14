d3f179c5251abbdde7d16cdb81db7881a31d5539
Emanuel Betcel <emanuelbetcel@gmail.com>
2022-10-14 16:10:15 -0300

    feat (generating csv file)

--- main.py
+++ main.py
@@ -1,96 +1,66 @@
-from itertools import groupby
 import apache_beam as beam
-import json
+import csv
+
+csv_header = "Regiao, Estado, UF, Governador, TotalCasos, TotalObitos"
+
 
 def select_columns_ibge(element):
     element.pop(2)
     element = element[0:3]
     return element[1], element
 
-# def select_columns_hist_painel_covid(element):
-#     indices = [0, 1, 3, 11, 13]
-#     selected_columns = [element[index] for index in indices]
 
-#     return selected_columns
-
-
-class create_column_columns_csv(beam.DoFn):
-    def __init__(self):
-        return
-    def process(self, line):
-        pass
-
-class sum_deaths(beam.DoFn):
-    def __init__(self):
-        return
-    def process(self, tup):
-        values = tup[1]
-        total_obitos = 0
-        total_casos = 0
-
-        for value in values:
-            total_obitos += int(value[4])
-            total_casos += int(value[3])
-        
-        values = [values[0][0], values[0][1], values[0][2], total_casos, total_obitos]
-        new_tuple_to_list = list(tup)
-        new_tuple_to_list[1] = values
-        yield new_tuple_to_list
-        
 with beam.Pipeline() as pipeline:
-    
-
     dados_covid = (
         pipeline
-        | "Read csv hist painel covid" >> beam.io.ReadFromText("hist_painel_covid.csv", skip_header_lines= True)
-        | "Split painel covid" >> beam.Map(lambda x: x.split(';'))
-        | "Select columns painel covid" >> beam.Map(lambda x: ((x[0], x[1], x[3]), (x[11], x[13])))
-        | 'Group by UF' >> beam.CombinePerKey(lambda l: (sum([int(a) for a, _ in l]), sum([int(b) for _, b in l])) )
-        # | 'Get grouped covid deaths' >> beam.Values()
-        # | 'Add the amounts for each group' >> beam.ParDo(sum_deaths())
-        | "Formmat output" >> beam.Map(lambda x: 
-        {
-        'coduf': x[0][2],
-        'Regiao': x[0][0], 
-        'UF': x[0][1], 
-        'TotalCasos': x[1][0], 
-        'TotalObitos' :x[1][1]
-        }
+        | "Read csv hist painel covid"
+        >> beam.io.ReadFromText("hist_painel_covid.csv", skip_header_lines=True)
+        | "Split painel covid" >> beam.Map(lambda x: x.split(";"))
+        | "Select columns painel covid"
+        >> beam.Map(lambda x: ((x[0], x[1], x[3]), (x[11], x[13])))
+        | "Group by UF"
+        >> beam.CombinePerKey(
+            lambda l: (sum([int(a) for a, _ in l]), sum([int(b) for _, b in l]))
         )
-        # | 'Group by UF code' >> beam.GroupBy(lambda s: s[2])
+        | "Formmat output"
+        >> beam.Map(
+            lambda x: {
+                "coduf": x[0][2],
+                "Regiao": x[0][0],
+                "UF": x[0][1],
+                "TotalCasos": x[1][0],
+                "TotalObitos": x[1][1],
+            }
+        )
     )
-
 
     estados_ibge = (
         pipeline
-        | "Read csv estados ibge" >> beam.io.ReadFromText("EstadosIBGE.csv", skip_header_lines=True)
-        | "Split" >> beam.Map(lambda x: x.split(';'))
-        | "Select columns" >> beam.Map(lambda x: (x[1],
-            {
-                'Estado': x[0],
-                'Governador': x[3]
-            }
-        )
-        )
-        )
+        | "Read csv estados ibge"
+        >> beam.io.ReadFromText("EstadosIBGE.csv", skip_header_lines=True)
+        | "Split" >> beam.Map(lambda x: x.split(";"))
+        | "Select columns"
+        >> beam.Map(lambda x: (x[1], {"Estado": x[0], "Governador": x[3]}))
+    )
 
     estados_dict = beam.pvalue.AsDict(estados_ibge)
 
     def join_dicts(a, b):
-        a.update(b.get(a['coduf']) or {})
+        a.update(b.get(a["coduf"]) or {})
         return a
-    joined_dicts = ( dados_covid 
-    | beam.Map(join_dicts, b=estados_dict)
-    # first_merge = ((estados_ibge, dados_covid)
-        # | 'Merge datasets' >> beam.CoGroupByKey()
-        | beam.FlatMap(print)
+
+    joined_dicts = dados_covid | beam.Map(join_dicts, b=estados_dict)
+
+    data_to_csv = (
+        joined_dicts
+        | "Unpack dict to csv format"
+        >> beam.Map(
+            lambda x: f'{x["Regiao"]},{x.get("Estado", "")},{x.get("UF", "")},{x.get("Governador", "")},{x["TotalCasos"]},{x["TotalObitos"]}'
+        )
+        | "Write CSV file"
+        >> beam.io.WriteToText(
+            "covid_brasil", file_name_suffix=".csv", header=csv_header
+        )
     )
-        # | beam.Map(lambda x: {
-        #     'Estado': x[1][0][0][0],
-        #     'Governador': x[1][0][0][2],
-        #     'UF': x[1][0][0]
 
-        # })
-    
-
-# pipeline.run()
+    data_to_json = joined_dicts | beam.Map(print)
