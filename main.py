64b1880d33f76ade5dfce6d568ed454a35b8e730
Emanuel Betcel <emanuelbetcel@gmail.com>
2022-10-13 22:20:33 -0300

    feat (finished dictionary)

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
+from itertools import groupby
 import apache_beam as beam
 import json
 
 def select_columns_ibge(element):
     element.pop(2)
     element = element[0:3]
-    return tuple(element)
+    return element[1], element
 
 # def select_columns_hist_painel_covid(element):
 #     indices = [0, 1, 3, 11, 13]
@@ -37,28 +38,59 @@ class sum_deaths(beam.DoFn):
         yield new_tuple_to_list
         
 with beam.Pipeline() as pipeline:
+    
+
+    dados_covid = (
+        pipeline
+        | "Read csv hist painel covid" >> beam.io.ReadFromText("hist_painel_covid.csv", skip_header_lines= True)
+        | "Split painel covid" >> beam.Map(lambda x: x.split(';'))
+        | "Select columns painel covid" >> beam.Map(lambda x: ((x[0], x[1], x[3]), (x[11], x[13])))
+        | 'Group by UF' >> beam.CombinePerKey(lambda l: (sum([int(a) for a, _ in l]), sum([int(b) for _, b in l])) )
+        # | 'Get grouped covid deaths' >> beam.Values()
+        # | 'Add the amounts for each group' >> beam.ParDo(sum_deaths())
+        | "Formmat output" >> beam.Map(lambda x: 
+        {
+        'coduf': x[0][2],
+        'Regiao': x[0][0], 
+        'UF': x[0][1], 
+        'TotalCasos': x[1][0], 
+        'TotalObitos' :x[1][1]
+        }
+        )
+        # | 'Group by UF code' >> beam.GroupBy(lambda s: s[2])
+    )
+
+
     estados_ibge = (
         pipeline
         | "Read csv estados ibge" >> beam.io.ReadFromText("EstadosIBGE.csv", skip_header_lines=True)
         | "Split" >> beam.Map(lambda x: x.split(';'))
-        | "Select columns" >> beam.Map(select_columns_ibge)
-        | beam.Map(print)
+        | "Select columns" >> beam.Map(lambda x: (x[1],
+            {
+                'Estado': x[0],
+                'Governador': x[3]
+            }
+        )
+        )
+        )
+
+    estados_dict = beam.pvalue.AsDict(estados_ibge)
+
+    def join_dicts(a, b):
+        a.update(b.get(a['coduf']) or {})
+        return a
+    joined_dicts = ( dados_covid 
+    | beam.Map(join_dicts, b=estados_dict)
+    # first_merge = ((estados_ibge, dados_covid)
+        # | 'Merge datasets' >> beam.CoGroupByKey()
+        | beam.FlatMap(print)
     )
+        # | beam.Map(lambda x: {
+        #     'Estado': x[1][0][0][0],
+        #     'Governador': x[1][0][0][2],
+        #     'UF': x[1][0][0]
 
-    # dados_covid = (
-    #     pipeline
-    #     | "Read csv hist painel covid" >> beam.io.ReadFromText("hist_painel_covid.csv", skip_header_lines= True)
-    #     | "Split painel covid" >> beam.Map(lambda x: x.split(';'))
-    #     | "Select columns painel covid" >> beam.Map(lambda x: [x[index] for index in [0, 1, 3, 11, 13]])
-    #     | 'Group by UF' >> beam.GroupBy(lambda x: x[2])
-    #     | 'Add the amounts for each group' >> beam.ParDo(sum_deaths())
-    #     | 'Get grouped covid deaths' >> beam.Values()
-    #     # | "Prepare columns" >> beam.Map()
-    # )
-
-    # first_merge = (
-    #     (estados_ibge, dados_covid)
-    #     | 'Merge datasets' >> beam.CoGroupByKey()
-    # )
+        # })
+    
 
 # pipeline.run()
